2d843c2105a9b1c251371b46489736438563d9b8
Rishab <shivam.jaiswal175@gmail.com>
2019-06-30 00:36:55 +0530

    Added kotlin, library & gradle upgrades. and new screen for image compression

---
+++ app/src/main/java/com/decomp/comp/decomp/CompressingImagesActivity.kt
@@ -0,0 +1,12 @@
+package com.decomp.comp.decomp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class CompressingImagesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_compressing_images)
+    }
+}
