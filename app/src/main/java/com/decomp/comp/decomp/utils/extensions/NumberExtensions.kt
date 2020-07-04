5ff22505a75a33dc85abb14f35e43438b1ba0229
Rishab <shivam.jaiswal175@gmail.com>
2020-07-04 15:18:52 +0530

    added number extension to get colorstate list

--- app/src/main/java/com/decomp/comp/decomp/utils/extensions/NumberExtensions.kt
+++ app/src/main/java/com/decomp/comp/decomp/utils/extensions/NumberExtensions.kt
@@ -1,8 +1,13 @@
 package com.decomp.comp.decomp.utils.extensions
 
 import android.content.Context
+import android.content.res.ColorStateList
 import androidx.core.content.ContextCompat
 
 fun Int.getColor(context: Context): Int {
     return ContextCompat.getColor(context, this)
+}
+
+fun Int.getColorStateList(context: Context): ColorStateList? {
+    return ContextCompat.getColorStateList(context, this)
 }
